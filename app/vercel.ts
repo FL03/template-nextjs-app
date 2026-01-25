1bbb82ce3b40c387cbfe93abf9b95d515313fa24
Joe McCain III <jo3mccain@icloud.com>
2026-01-25 11:29:18 -0600

    update
    Signed-off-by: Joe McCain III <jo3mccain@icloud.com>

--- app/vercel.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelConfig } from '@vercel/config/v1';
-
-export const config: VercelConfig = {
-  bunVersion: '1.x',
-  outputDirectory: 'build',
-  buildCommand: "bun run --bun next build",
-  devCommand: "bun run --bun next dev",
-};
